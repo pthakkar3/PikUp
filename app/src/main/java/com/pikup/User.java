20417ed80d53633f59746bd9d51b59d962f1bfa3
Altan <astalker3@gatech.edu>
2017-09-08 00:04:27 -0400

    Added support for user profiles
    New User class that interfaces with Firebase.
    
    New layout for Profile edit page

---
+++ app/src/main/java/com/pikup/User.java
@@ -0,0 +1,73 @@
+package com.pikup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by Altan on 9/7/2017.
+ */
+
+public class User {
+
+    private String displayName;
+    private int age;
+    private String gender;
+    private Long phoneNumber;
+    private boolean isStudent;
+
+    public User(String displayName, int age, String gender, Long phoneNumber, boolean isStudent) {
+
+        this.displayName = displayName;
+        this.age = age;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.isStudent = isStudent;
+
+    }
+
+    public User() {
+
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean getIsStudent() {
+        return isStudent;
+    }
+
+    public void setStudent(boolean student) {
+        isStudent = student;
+    }
+}
