4d8de7f167abe0ff48d69139030f7859dfb8e6e6
Altan <astalker3@gatech.edu>
2017-11-02 20:46:03 -0400

    Made the map on the home screen functional

--- app/src/main/java/com/pikup/ui/HomeScreenActivity.java
+++ app/src/main/java/com/pikup/ui/HomeScreenActivity.java
@@ -1,13 +1,33 @@
 package com.pikup.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,21 +35,69 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.location.LocationServices;
 import com.pikup.R;
 import com.pikup.model.User;
 
-public class HomeScreenActivity extends AppCompatActivity {
+
+public class HomeScreenActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+
+    private MapView mapView;
+    private GoogleMap map;
+    private LatLng userLatLng;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        MapsInitializer.initialize(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+
+                try {
+                    googleMap.setMyLocationEnabled(true);
+                    LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    Criteria criteria = new Criteria();
+                    String provider = locationManager.getBestProvider(criteria, true);
+                    Location location = locationManager.getLastKnownLocation(provider);
+                    if (location != null) {
+                        double lat = location.getLatitude();
+                        double lng = location.getLongitude();
+
+                        LatLng latLng = new LatLng(lat, lng);
+                        userLatLng = latLng;
+
+                        googleMap.addMarker(new MarkerOptions().position(userLatLng));
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 15));
+                    }
+                } catch (SecurityException e) {
+                    Log.e("ERROR:", e.toString());
+                }
+
+                mapView.onResume();
+        }});
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
         DatabaseReference userRef = mDatabase.child("userList").child(mAuth.getCurrentUser().getUid());
         userRef.addValueEventListener(new ValueEventListener() {
@@ -55,13 +123,22 @@ public class HomeScreenActivity extends AppCompatActivity {
 
     }
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
 
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
     public void viewProfile(View view) {
         Intent intent = new Intent(this, UserProfileActivity.class);
         startActivity(intent);
         finish();
     }
+
     public void logOut(View view) {
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(this, WelcomeScreenActivity.class);
@@ -79,5 +156,45 @@ public class HomeScreenActivity extends AppCompatActivity {
         Intent intent = new Intent(this, JoinListActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
